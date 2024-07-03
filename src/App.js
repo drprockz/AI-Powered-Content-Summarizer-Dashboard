5fe6607271333738ab82fc1bf10e402df012971b
Darshan Parmar <119973173+drprockz@users.noreply.github.com>
2024-07-03 19:45:13 +0530

    Add React Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>My React App</h1>
+            <p>This is a custom React setup without create-react-app.</p>
+        </div>
+    );
+};
+
+export default App;
